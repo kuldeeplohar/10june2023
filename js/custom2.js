f5aa5c6799a07f1054d7270eed239e3ada8d45bc
kuldeeplohar <loharkuldeep169@gmail.com>
2023-06-23 14:24:29 +0530

    10june2023

---
+++ js/custom2.js
@@ -0,0 +1,5 @@
+   // concatenation operator
+
+ console.log("acording "+"to"+' Naveen'+' Mishra'+" who"+' head'+' the '+"Haryana\'s "+
+ "'super "+"100'"+' program'+ 'at'+' rewari,'+' of'+` the`+` 40`+` student `+"who"+' qualified'+" the"+' jee'+' Advanced, '
+ +' 33'+' are '+ 'boys '+'and '+'seven '+'are'+" girls.")
---
+++ js/custom4.js
@@ -0,0 +1,6 @@
+
+
+console.log("Kuldeep "+"Lohar");  //string + string
+console.log("kuldeep"+11);       //string + Number
+console.log(10+20);              //number + number
+console.log("30"+"40");          //string + string 
